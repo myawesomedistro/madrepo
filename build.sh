777d45ec43ded0b3745951e208d23e48321b99cc
Raul Dipeas <rauldipeas@mail.com>
2020-11-06 11:22:22 -0300

    ^build

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-sudo add-apt-repository ppa:apt-fast/stable
-sudo apt install -y apt-fast checkinstall dpkg-sig reprepro sshpass
+sudo apt install -y checkinstall dpkg-sig reprepro sshpass
 mkdir -pv tmp/packages
-bash mkpkg/*.sh
-bash getpkg/*.sh
+for f in mkpkg/*.sh
+do
+ bash $f
+done
+for f in getpkg/*.sh
+do
+ bash $f
+done
 mv *.deb tmp/packages
 find tmp/packages -name '*.deb' -type f|xargs dpkg-sig --sign builder
 find tmp/packages -name '*.deb' -type f|xargs reprepro -P --ask-passphrase -Vb repo includedeb core
