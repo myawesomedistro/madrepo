d0230185d733bbe0a08fa23ba36e724ede7ed996
Raul Dipeas <rauldipeas@mail.com>
2020-11-07 07:16:14 -0300

    ^getpkg

--- getpkg/wine.sh
+++ getpkg/wine.sh
@@ -3,3 +3,4 @@ echo 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main' | sudo tee /e
 wget -qO- https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/winehq.gpg>/dev/null
 sudo apt update
 grep ^Package /var/lib/apt/lists/*wine*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+grep ^Package /var/lib/apt/lists/*wine*i386*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
