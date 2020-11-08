4512b4c7fcc80d7b4b30a8ee9fae0370ab8101ce
Raul Dipeas <rauldipeas@mail.com>
2020-11-08 12:53:54 -0300

    ^getpkg

--- getpkg/megasync.sh
+++ getpkg/megasync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -q http://ftp.us.debian.org/debian/pool/main/libr/libraw/$(wget -qO- http://ftp.us.debian.org/debian/pool/main/libr/libraw/|grep amd64.deb|grep libraw19|head -n1|cut -d '"' -f8)
+#aria2c http://ftp.us.debian.org/debian/pool/main/libr/libraw/$(wget -qO- http://ftp.us.debian.org/debian/pool/main/libr/libraw/|grep amd64.deb|grep libraw19|head -n1|cut -d '"' -f8)
+aria2c http://ftp.us.debian.org/debian/pool/main/libr/libraw/libraw19_0.19.2-2_amd64.deb
