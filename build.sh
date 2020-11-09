2911b4e7aa11d811779d8181fc1135fafa651b1c
Raul Dipeas <rauldipeas@mail.com>
2020-11-09 02:07:54 -0300

    ^azure-pipelines ^build ^distributions

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-sudo sed -i 's/_APTMGR=apt-get/_APTMGR=apt/g' /etc/apt-fast.conf
-sudo apt-fast install -y checkinstall dpkg-sig reprepro sshpass
-sudo dpkg --add-architecture i386
 mkdir -pv tmp/packages
+
 for script in mkpkg/*.sh
 do
  bash $script
 done
+
 for script in getpkg/*.sh
 do
  bash $script
 done
+
 mv *.deb tmp/packages
+
 find tmp/packages -name '*.deb' -type f|xargs dpkg-sig --sign builder
 find tmp/packages -name '*.deb' -type f|xargs reprepro -P --ask-passphrase -Vb repo includedeb core
