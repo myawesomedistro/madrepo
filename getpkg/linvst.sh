024dfe851acdf33db109e61b82d32c9e6592f60a
Raul Dipeas <rauldipeas@mail.com>
2020-11-10 06:41:41 -0300

    ^getpkg

--- getpkg/linvst.sh
+++ getpkg/linvst.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-aria2c $(wget -qO- https://api.github.com/repos/osxmidi/LinVst/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)
+aria2c $(wget -qO- https://api.github.com/repos/osxmidi/LinVst/releases|grep browser_download_url|grep 64bit|grep .deb|head -n1|cut -d '"' -f4)
+aria2c $(wget -qO- https://api.github.com/repos/osxmidi/LinVst/releases|grep browser_download_url|grep packages|head -n1|cut -d '"' -f4)
+unzip LinVst*packages*.zip
+cp LinVst*packages*/*/*.deb .
