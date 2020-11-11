e26500c26e2bf56ade367e6d1a3dd3a26c291621
Raul Dipeas <rauldipeas@mail.com>
2020-11-11 07:27:35 -0300

    ^mkpkg

---
+++ mkpkg/rofi.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+ROFI_TAG=$(wget -qO- https://api.github.com/repos/davatorium/rofi/releases|grep tag|head -n1|cut -d \" -f4|sed 's/https:\/\/github.com\/davatorium\/rofi\/releases\/tag\///g')
+sudo apt install -y \
+    bison \
+    debhelper \
+    dh-autoreconf \
+    dh-strip-nondeterminism \
+    flex \
+    gir1.2-harfbuzz-0.0 \
+    icu-devtools \
+    libbison-dev \
+    libcairo-script-interpreter2 \
+    libcairo2-dev \
+    libfile-stripnondeterminism-perl \
+    libgdk-pixbuf2.0-dev \
+    libgraphite2-dev \
+    libharfbuzz-dev \
+    libharfbuzz-gobject0 \
+    libice-dev \
+    libicu-dev \
+    libicu-le-hb-dev \
+    libicu-le-hb0 \
+    libjpeg-dev \
+    libpango1.0-dev \
+    libpixman-1-dev \
+    libpthread-stubs0-dev \
+    librsvg2-dev \
+    libsm-dev \
+    libstartup-notification0-dev \
+    libx11-dev \
+    libxau-dev \
+    libxcb-ewmh-dev \
+    libxcb-icccm4-dev \
+    libxcb-randr0-dev \
+    libxcb-render0-dev \
+    libxcb-shm0-dev \
+    libxcb-util-dev \
+    libxcb-util0-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxcb-xrm-dev \
+    libxcb1-dev \
+    libxdmcp-dev \
+    libxext-dev \
+    libxft-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    libxrender-dev \
+    m4 \
+    po-debconf \
+    x11proto-core-dev \
+    x11proto-dev \
+    x11proto-xext-dev \
+    xorg-sgml-doctools \
+    xtrans-dev
+git clone https://github.com/davatorium/rofi
+cd rofi
+git submodule update --init
+autoreconf -i
+./configure
+make
+sudo checkinstall -D -y \
+    --install=no \
+    --maintainer davatorium@github.com \
+    --pkgversion $ROFI_TAG \
+    --requires 'libxcb-ewmh2, libxcb-xrm0'
+cd ..
+cp rofi/rofi*.deb tmp/packages
