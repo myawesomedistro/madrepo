0c813a4a0310ec3388a18d3a1d9ddfeba2600947
Raul Dipeas <rauldipeas@mail.com>
2020-11-12 18:52:01 -0300

    ^mkpkg

--- mkpkg/bashrun.sh
+++ mkpkg/bashrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+BASHRUN_TAG=`echo $(git describe --always --dirty)-$(git log -1 --date=short --pretty=format:%cd)|sed 's/v//g'|sed 's/_/-/g'`
 git clone https://gitlab.com/myawesomedistro/bashrun
 rm -rf bashrun/.git
+sed -i "s/TAG/$BASHRUN_TAG" bashrun/DEBIAN/control
 dpkg-deb -b bashrun .
 cp bashrun*.deb tmp/packages
--- mkpkg/madwelcome.sh
+++ mkpkg/madwelcome.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+MADWELCOME_TAG=`echo $(git describe --always --dirty)-$(git log -1 --date=short --pretty=format:%cd)|sed 's/v//g'|sed 's/_/-/g'`
 git clone https://gitlab.com/myawesomedistro/madwelcome
 rm -rf madwelcome/.git
+sed -i "s/TAG/$MADWELCOME_TAG" bashrun/DEBIAN/control
 dpkg-deb -b madwelcome .
 cp madwelcome*.deb tmp/packages
