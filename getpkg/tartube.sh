c30325393381cbca648008ded4b423105465ddad
Raul Dipeas <rauldipeas@mail.com>
2020-11-17 23:53:44 -0300

    ^getpkg

---
+++ getpkg/tartube.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TARTUBE_URL="https://sourceforge.net/projects/tartube/files"
+TARTUBE_VER=`wget -O- https://sourceforge.net/projects/tartube/files|grep 'files/v'|grep download|head -n1|cut -d '/' -f5`
+TARTUBE_FILE=`wget -O- $TARTUBE_URL/$TARTUBE_VER|grep STRICT.deb|head -n1|cut -d '"' -f2`
+aria2c $TARTUBE_URL/$TARTUBE_VER/$TARTUBE_FILE
