7873b8ee57801da370713d9a5fb8dfa80fb3c4da
Raul Dipeas <rauldipeas@mail.com>
2020-11-25 10:03:15 -0300

    ^getpkg ^mkpkg

--- getpkg/deb-multimedia.sh
+++ getpkg/deb-multimedia.sh
@@ -3,5 +3,5 @@ echo 'deb https://deb-multimedia.org bullseye main non-free'|sudo tee /etc/apt/s
 aria2c http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/$(wget -qO- http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/|grep all.deb|head -n1|cut -d \" -f2)
 sudo apt install -y --no-install-recommends ./deb-multimedia-keyring*.deb 2>/dev/null
 sudo apt update
-grep ^Package /var/lib/apt/lists/*deb-multimedia*amd64_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
-grep ^Package /var/lib/apt/lists/*deb-multimedia*i386_Packages|awk '{print $2}'|sort -u|sed 's/$/:i386/g'|xargs apt-fast download
+grep ^Package /var/lib/apt/lists/*deb-multimedia*amd64*Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+grep ^Package /var/lib/apt/lists/*deb-multimedia*i386*Packages|awk '{print $2}'|sort -u|sed 's/$/:i386/g'|xargs apt-fast download
