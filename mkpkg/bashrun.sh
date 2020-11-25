e75ef8ed14456887cc936cd953d56a7fdd4125e8
Raul Dipeas <rauldipeas@mail.com>
2020-11-25 10:58:14 -0300

    ^mkpkg

--- mkpkg/bashrun.sh
+++ mkpkg/bashrun.sh
@@ -2,7 +2,9 @@
 git clone https://gitlab.com/myawesomedistro/bashrun
 cd bashrun
 cat usr/bin/bashrun-url|sed "s/BASHRUN_GL_TOKEN/$BASHRUN_GL_TOKEN/g"|tee usr/bin/bashrun-url
-shc -f usr/bin/bashrun-url -o usr/bin/bashrun-url
+shc -f usr/bin/bashrun-url -o bashrun-url
+mv bashrun-url usr/bin/bashrun-url
+rm bashrun-url.x.c
 BASHRUN_TAG=`echo $(git describe --always --dirty)-$(git log -1 --date=iso --pretty=format:%cd)|sed 's/\ /-/g'|sed 's/v//g'|sed 's/_/-/g'|sed 's/:/-/g'|sed 's/dirty-//g'|cut -d '-' -f2-7`
 sed -i "s/TAG/$BASHRUN_TAG/g" DEBIAN/control
 rm -rf .git
