143ac21116eaa856d97ef4986a3781566f7636d4
Raul Dipeas <rauldipeas@mail.com>
2020-11-27 22:21:07 -0300

    ^getpkg

---
+++ getpkg/haguichi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:webupd8team/haguichi
+grep ^Package /var/lib/apt/lists/*haguichi*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+aria2c https://www.vpn.net$(wget -qO- https://www.vpn.net/linux|grep amd64.deb|cut -d '"' -f6)
