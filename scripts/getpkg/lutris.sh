6b5b3130a9c79223faa89aeaac4c0602ef7abbdd
Raul Dipeas <rauldipeas@mail.com>
2020-12-02 11:39:18 -0300

    ^getpkg

--- scripts/getpkg/lutris.sh
+++ scripts/getpkg/lutris.sh
@@ -3,4 +3,4 @@
 echo 'deb http://download.opensuse.org/repositories/home:/strycore/Debian_Testing/ ./'|sudo tee /etc/apt/sources.list.d/lutris.list
 wget -qO- https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/lutris.gpg>/dev/null
 sudo apt update
-grep ^Package /var/lib/apt/lists/*lutris*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+grep ^Package /var/lib/apt/lists/*strycore*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
