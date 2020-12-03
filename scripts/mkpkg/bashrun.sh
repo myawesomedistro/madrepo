a77e9212eae947c5e167d83707c0bdc107831935
Raul Dipeas <rauldipeas@mail.com>
2020-12-03 19:47:17 -0300

    ^mkpkg

--- scripts/mkpkg/bashrun.sh
+++ scripts/mkpkg/bashrun.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-#TOKEN=`echo $BASHRUN_GL_TOKEN`
-#git clone https://gitlab.com/myawesomedistro/bashrun
-#cd bashrun
-#sed -i "s/BASHRUN_GL_TOKEN/$TOKEN/g" usr/bin/bashrun-url
-#shc -f usr/bin/bashrun-url -o usr/bin/bashrun-url
-#rm usr/bin/bashrun-url.x.c
-#BASHRUN_TAG=`echo $(git describe --always --dirty)-$(git log -1 --date=iso --pretty=format:%cd)|sed 's/\ /-/g'|sed 's/v//g'|sed 's/_/-/g'|sed 's/:/-/g'|sed 's/dirty-//g'|cut -d '-' -f2-7`
-#sed -i "s/TAG/$BASHRUN_TAG/g" DEBIAN/control
-#rm -rf .git
-#cd ..
-#dpkg-deb -b bashrun .
-aria2c -o bashrun.deb http://my.opendesktop.org/s/KRQ9izGPxtsjMMs/download
+git clone https://gitlab.com/myawesomedistro/bashrun
+cd bashrun
+BASHRUN_TAG=`echo $(git describe --always --dirty)-$(git log -1 --date=iso --pretty=format:%cd)|sed 's/\ /-/g'|sed 's/v//g'|sed 's/_/-/g'|sed 's/:/-/g'|sed 's/dirty-//g'|cut -d '-' -f2-7`
+sed -i "s/TAG/$BASHRUN_TAG/g" DEBIAN/control
+rm -rf .git
+cd ..
+dpkg-deb -b bashrun .
+#aria2c -o bashrun.deb http://my.opendesktop.org/s/KRQ9izGPxtsjMMs/download
 mv bashrun*.deb tmp/packages
