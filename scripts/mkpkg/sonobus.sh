f8a2e4cefab48b8426d4a25d73fce5ca17ef5566
Raul Dipeas <rauldipeas@mail.com>
2020-12-24 08:14:40 -0300

    ^mkpkg

--- scripts/mkpkg/sonobus.sh
+++ scripts/mkpkg/sonobus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-fast install -y --fix-missing \
     libasound2-dev \
-    libcurl4-gnutls-dev \
+    libcurl4-openssl-dev \
     libfreetype-dev \
     libgl-dev \
     libjack-jackd2-dev \
@@ -23,8 +23,8 @@ mkdir -pv \
     sonobus/usr/bin \
     sonobus/usr/share/applications \
     sonobus/usr/share/pixmaps
-cp build/SonoBus sonobus/usr/bin/SonoBus
-cp build/SonoBus.vst3 sonobus/usr/lib/lxvst/SonoBus
+cp -rf build/SonoBus sonobus/usr/bin/SonoBus
+cp -rf build/SonoBus.vst3 sonobus/usr/lib/lxvst/SonoBus
 cp sonobus.desktop sonobus/usr/share/applications/sonobus.desktop
 cp ../../images/SonoBus-Icon.svg sonobus/usr/share/pixmaps/sonobus.svg
 SONOBUS_TAG=`echo $(wget -qO- https://api.github.com/repos/essej/sonobus/releases|grep tag|grep -v Next|head -n1|cut -d \" -f4|sed 's/https:\/\/github.com\/essej\/sonobus\/releases\/tag\///g'|sed 's/v//g')`
