45339f01e124c38edaf985e64cee4395f2e88643
Raul Dipeas <rauldipeas@mail.com>
2021-01-08 18:35:04 -0300

    ^getpkg

--- scripts/getpkg/heroic.sh
+++ scripts/getpkg/heroic.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+aria2c $(wget -qO- https://api.github.com/repos/derrod/legendary/releases|grep browser_download_url|grep all.deb|head -n1|cut -d '"' -f4)
 aria2c $(wget -qO- https://api.github.com/repos/flavioislima/HeroicGamesLauncher/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)
