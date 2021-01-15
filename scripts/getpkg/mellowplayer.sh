abe11d700ca8f2b8f4442017aae996d9916dc54c
Raul Dipeas <rauldipeas@mail.com>
2021-01-15 00:20:19 -0300

    ^getpkg

--- scripts/getpkg/mellowplayer.sh
+++ scripts/getpkg/mellowplayer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'deb http://download.opensuse.org/repositories/home:/ColinDuquesnoy/Debian_Unstable/ /'|sudo tee /etc/apt/sources.list.d/mellowplayer.list
-wget -qO- https://download.opensuse.org/repositories/home:ColinDuquesnoy/Debian_Unstable/Release.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/mellowplayer.gpg>/dev/null
+wget -qO- https://download.opensuse.org/repositories/home:/ColinDuquesnoy/Debian_Unstable/Release.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/mellowplayer.gpg>/dev/null
 sudo apt update
 grep ^Package /var/lib/apt/lists/*ColinDuquesnoy*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
