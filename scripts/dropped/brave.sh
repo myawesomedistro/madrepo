3caa7b3182a3b34dcb89e2e4806586491106f7d2
Raul Dipeas <rauldipeas@mail.com>
2021-01-17 04:14:21 -0300

    ^getpkg

--- scripts/dropped/brave.sh
+++ scripts/dropped/brave.sh
@@ -3,4 +3,3 @@ echo 'deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable ma
 wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-core.asc|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/brave.gpg>/dev/null
 sudo apt update
 grep ^Package /var/lib/apt/lists/*brave*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
-rm -rfv brave-keyring*.deb
