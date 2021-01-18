842c533a9631352153d4617d0e9cb5b3215e4d89
Raul Dipeas <rauldipeas@mail.com>
2021-01-18 07:43:07 -0300

    ^madrepo ^getpkg

--- scripts/getpkg/github-desktop.sh
+++ scripts/getpkg/github-desktop.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo 'deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main'|sudo tee /etc/apt/sources.list.d/github-desktop.list
-wget -qO- https://packagecloud.io/shiftkey/desktop/gpgkey|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/github-desktop.gpg>/dev/null
-sudo apt update
-grep ^Package /var/lib/apt/lists/*shiftkey*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+aria2c $(wget -qO- https://api.github.com/repos/shiftkey/desktop/releases|grep browser_download_url|grep download|grep .deb|head -n1|cut -d '"' -f4)
