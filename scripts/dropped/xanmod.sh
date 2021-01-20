88f290c7f5017d661a7693818027d4f7d5cb9e15
Raul Dipeas <rauldipeas@mail.com>
2021-01-20 14:53:23 -0300

    ^dropped

--- scripts/dropped/xanmod.sh
+++ scripts/dropped/xanmod.sh
@@ -2,8 +2,5 @@
 wget https://dl.xanmod.org/xanmod-repository.deb
 sudo apt-fast install -y --no-install-recommends ./xanmod*.deb
 rm -rf xanmod*.deb
-# Remoção dos repositórios do Azure
-#echo ''|sudo tee /etc/apt/sources.list
-#sudo rm -rf /etc/apt/sources.list.d/*
 sudo apt update
 grep ^Package /var/lib/apt/lists/*xanmod*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
