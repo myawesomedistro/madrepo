59a0a7ae6683060d62fa270e8ef88bfcb2dab7fc
Raul Dipeas <rauldipeas@mail.com>
2021-01-23 16:16:24 -0300

    *stats

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget -O- https://sourceforge.net/projects/madlinux/files/repo/dists/core/main/binary-amd64/Packages|grep 'Package:\|Version:'|sed ':a;N;$!ba;s/\nVersion://g'|sed 's/Package: //g'>repo/packages-amd64-`date +'%d-%m-%Y-%H-%M'`.log
-wget -O- https://sourceforge.net/projects/madlinux/files/repo/dists/core/main/binary-i386/Packages|grep 'Package:\|Version:'|sed ':a;N;$!ba;s/\nVersion://g'|sed 's/Package: //g'>repo/packages-i386-`date +'%d-%m-%Y-%H-%M'`.log
 mkdir -pv tmp/packages
 for script in scripts/mkpkg/*.sh
 do
@@ -12,9 +10,3 @@ do
 done
 mv *.deb tmp/packages
 find tmp/packages -name '*.deb' -type f|xargs reprepro -P --ask-passphrase -Vb repo includedeb core
-cat repo/dists/core/main/binary-amd64/Packages|grep 'Package:\|Version:'|sed ':a;N;$!ba;s/\nVersion://g'|sed 's/Package: //g'>repo/packages-amd64-`date +'%d-%m-%Y-%H-%M'`.log
-cat repo/dists/core/main/binary-i386/Packages|grep 'Package:\|Version:'|sed ':a;N;$!ba;s/\nVersion://g'|sed 's/Package: //g'>repo/packages-i386-`date +'%d-%m-%Y-%H-%M'`.log
-diff --new-line-format="+ %L" --old-line-format="- %L" --unchanged-line-format="= %L" repo/packages-amd64*|grep '+ ' > packages.log
-diff --new-line-format="+ %L" --old-line-format="- %L" --unchanged-line-format="= %L" repo/packages-amd64*|grep '\- ' >> packages.log
-diff --new-line-format="+ %L" --old-line-format="- %L" --unchanged-line-format="%L" repo/packages-i386*|grep '+ ' >> packages.log
-diff --new-line-format="+ %L" --old-line-format="- %L" --unchanged-line-format="%L" repo/packages-i386*|grep '\- ' >> packages.log7
