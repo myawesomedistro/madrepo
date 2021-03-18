475efd6368c4144648cb28a76287bd64604eb1e1
Raul Dipeas <rauldipeas@mail.com>
2021-03-18 17:12:17 -0300

    ^getpkg ^dropped

--- scripts/getpkg/jellyfin.sh
+++ scripts/getpkg/jellyfin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo 'deb [arch=amd64] https://repo.jellyfin.org/debian bullseye main'|sudo tee /etc/apt/sources.list.d/jellyfin.list
-wget -qO- https://repo.jellyfin.org/jellyfin_team.gpg.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/jellyfin.gpg
+wget -qO- https://repo.jellyfin.org/jellyfin_team.gpg.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/jellyfin.gpg>/dev/null
 apt-fast update
 grep ^Package: /var/lib/apt/lists/*jellyfin*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
