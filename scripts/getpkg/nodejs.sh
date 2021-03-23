4285194f31e50cd0e49ad487e16eddb70e637c21
Raul Dipeas <rauldipeas@mail.com>
2021-03-23 10:01:24 -0300

    ^madrepo ^getpkg

--- scripts/getpkg/nodejs.sh
+++ scripts/getpkg/nodejs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-wget -qO- https://deb.nodesource.com/setup_15.x|sudo bash
+wget -qO- https://deb.nodesource.com/setup_14.x|sudo bash
 grep ^Package: /var/lib/apt/lists/*node*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
