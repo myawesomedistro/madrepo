a0e79fa89fddc4c38bc4ab24ad7960f096530c43
Raul Dipeas <rauldipeas@mail.com>
2021-03-25 03:40:43 -0300

    ^getpkg

---
+++ scripts/getpkg/vscode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg
+echo 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'|sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+grep ^Package: /var/lib/apt/lists/*vscode*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
