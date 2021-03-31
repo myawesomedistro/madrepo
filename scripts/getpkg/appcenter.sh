6ae016e7677602f5fc183a123d29a2801e24d5ef
Raul Dipeas <rauldipeas@mail.com>
2021-03-31 02:23:38 -0300

    ^getpkg

--- scripts/getpkg/appcenter.sh
+++ scripts/getpkg/appcenter.sh
@@ -4,6 +4,7 @@ echo 'deb http://packages.elementary.io/appcenter bionic main'|sudo tee /etc/apt
 wget -qO- http://packages.elementary.io/key.asc|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/appcenter.gpg>/dev/null
 apt-fast update
 grep ^Package: /var/lib/apt/lists/*appcenter*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
-sudo add-apt-repository ppa:elementary-os/stable
+sudo add-apt-repository -n ppa:elementary-os/stable
+sudo sed -i 's/focal/bionic/g' /etc/apt/sources.list.d/elementary*
 sudo add-apt-repository ppa:elementary-os/daily
 grep ^Package: /var/lib/apt/lists/*elementary-os*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
