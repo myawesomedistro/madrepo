90d001c2c6f0633c6710cf9a0feda88cc9a17f94
Raul Dipeas <rauldipeas@mail.com>
2021-03-31 01:44:01 -0300

    ^getpkg

--- scripts/getpkg/appcenter.sh
+++ scripts/getpkg/appcenter.sh
@@ -4,5 +4,6 @@ echo 'deb http://packages.elementary.io/appcenter bionic main'|sudo tee /etc/apt
 wget -qO- http://packages.elementary.io/key.asc|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/appcenter.gpg>/dev/null
 apt-fast update
 grep ^Package: /var/lib/apt/lists/*appcenter*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+sudo add-apt-repository ppa:elementary-os/stable
 sudo add-apt-repository ppa:elementary-os/daily
 grep ^Package: /var/lib/apt/lists/*elementary-os*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
