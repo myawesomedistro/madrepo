eea49f483cee5932ded6d487f6a4769ea14189e3
Raul Dipeas <rauldipeas@mail.com>
2021-04-04 15:04:22 -0300

    ^getpkg

--- scripts/getpkg/linux-firmware.sh
+++ scripts/getpkg/linux-firmware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-aria2c http://deb.xanmod.org/pool/main/l/linux-firmware/$(wget -qO- http://deb.xanmod.org/pool/main/l/linux-firmware/|grep linux-firmware|cut -d '"' -f8|tail -n1)
+aria2c http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/$(wget -qO- http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/|grep linux-firmware|grep all.deb|tail -n3|head -n1|cut -d '"' -f2)
