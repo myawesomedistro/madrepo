7392b5b7f056c506a0d78f898eecc353de28267c
Raul Dipeas <rauldipeas@mail.com>
2021-04-07 03:06:23 -0300

    ^mkpkg

--- scripts/mkpkg/google-docs.sh
+++ scripts/mkpkg/google-docs.sh
@@ -5,7 +5,7 @@ sudo npm install -g nativefier
 wget https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Google_Docs_icon_%282020%29.svg/250px-Google_Docs_icon_%282020%29.svg.png -O google-docs.png
 nativefier -p linux -a x64 -i google-docs.png --disable-context-menu --disable-dev-tools --single-instance --internal-urls ".*" -f --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0" --internal-urls "(.*?docs\.google\.com.*?|.*?accounts\.google\.com.*?)" 'https://www.docs.google.com'
 mkdir -p google-docs/{DEBIAN,opt,usr/share/applications,usr/share/pixmaps}
-mv FazerloginnasContasdoGoogle-linux-x64/ google-docs/opt/google-docs
+mv Signin-GoogleAccounts-linux-x64/ google-docs/opt/google-docs
 mv google-docs.png google-docs/usr/share/pixmaps/google-docs.png
 ELECTRON_RELEASE=`wget -qO- https://raw.githubusercontent.com/nativefier/nativefier/master/src/constants.ts|grep ELECTRON_VERSION|head -n1|cut -d "'" -f2`
 cat <<EOF |tee google-docs/DEBIAN/control
@@ -27,7 +27,7 @@ Comment=Access Google Docs on Linux.
 Name=Google Docs
 GenericName=Google Docs desktop app
 Icon=google-docs
-Exec=/opt/google-docs/FazerloginnasContasdoGoogle
+Exec=/opt/google-docs/Signin-GoogleAccounts
 Categories=Network
 EOF
 dpkg-deb -b google-docs .
