7ff6b8b2a5fe3a089cef8fd3c596cfcfb13c0879
Raul Dipeas <rauldipeas@mail.com>
2021-04-11 13:04:39 -0300

    ^assets ^getpkg

--- scripts/getpkg/sendanywhere.sh
+++ scripts/getpkg/sendanywhere.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
-aria2c https://update.send-anywhere.com/linux_downloads/sendanywhere_latest_amd64.deb
+if wget https://update.send-anywhere.com/linux_downloads/sendanywhere_latest_amd64.deb 2>/dev/null
+then
+   echo "Download do Send Anywhere concluído com sucesso!"
+else
+   echo "O Download do Send Anywhere falhou, copiano um arquivo local..."
+   cp -v assets/sendanywhere*.deb .
+fi
