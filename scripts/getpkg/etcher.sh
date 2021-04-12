3b9e82b32cf15233bb1decaea0adb74758469bae
Raul Dipeas <rauldipeas@mail.com>
2021-04-12 16:51:56 -0300

    ^getpkg

--- scripts/getpkg/etcher.sh
+++ scripts/getpkg/etcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-echo 'deb https://deb.etcher.io stable etcher'|sudo tee /etc/apt/sources.list.d/balena-etcher.list
-sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-apt-fast update
-grep ^Package: /var/lib/apt/lists/*etcher*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+#echo 'deb https://deb.etcher.io stable etcher'|sudo tee /etc/apt/sources.list.d/balena-etcher.list
+#sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
+#apt-fast update
+#grep ^Package: /var/lib/apt/lists/*etcher*_Packages|awk '{print $2}'|sort -u|xargs apt-fast download
+aria2c $(wget -qO- https://api.github.com/repos/balena-io/etcher/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)
