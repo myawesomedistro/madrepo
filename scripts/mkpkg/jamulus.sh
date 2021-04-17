2ffb05f7b515e2433cd4f36bce23e8788751293d
Raul Dipeas <rauldipeas@mail.com>
2021-04-17 13:02:57 -0300

    ^mkpkg

--- scripts/mkpkg/jamulus.sh
+++ scripts/mkpkg/jamulus.sh
@@ -2,6 +2,10 @@
 set -e
 git clone https://github.com/corrados/jamulus
 cd jamulus/distributions
+apt-fast install -y \
+	libjack-jackd2-dev \
+	qtbase5-dev \
+	qttools5-dev-tools
 bash build-debian-package.sh
 cd ../..
 mv jamulus*.deb tmp/packages
