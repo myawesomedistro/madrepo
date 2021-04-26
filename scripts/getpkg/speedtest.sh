21856178de9f4c229b948dfc5b66c8f536a39332
Raul Dipeas <rauldipeas@mail.com>
2021-04-26 12:21:54 -0300

    ^getpkg

--- scripts/getpkg/speedtest.sh
+++ scripts/getpkg/speedtest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-export INSTALL_KEY=379CE192D401AB61
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo 'deb https://ookla.bintray.com/debian generic main'|sudo tee /etc/apt/sources.list.d/speedtest.list
+#export INSTALL_KEY=379CE192D401AB61
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+#echo 'deb https://ookla.bintray.com/debian generic main'|sudo tee /etc/apt/sources.list.d/speedtest.list
+wget -O- https://packagecloud.io/ookla/speedtest-cli/gpgkey|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/speedtest.gpg
+echo 'deb https://packagecloud.io/ookla/speedtest-cli/debian/ bullseye main'|sudo tee /etc/apt/sources.list.d/speedtest.list
 apt-fast update
-cat /var/lib/apt/lists/*ookla*_Packages|grep ^Package:|awk '{print $2}'|sort -u|xargs apt-fast download
+cat /var/lib/apt/lists/*speedtest*_Packages|grep ^Package:|awk '{print $2}'|sort -u|xargs apt-fast download
