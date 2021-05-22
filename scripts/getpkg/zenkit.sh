b8c2c085878cf4b187895a0accba2ec55807462d
Raul Dipeas <rauldipeas@mail.com>
2021-05-22 11:06:11 -0300

    ^getpkg

--- scripts/getpkg/zenkit.sh
+++ scripts/getpkg/zenkit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
 aria2c https://static.zenkit.com/downloads/desktop-apps/base/zenkit-base-linux.deb
+aria2c https://static.zenkit.com/downloads/desktop-apps/chat/zenchat-linux.deb
+aria2c https://static.zenkit.com/downloads/desktop-apps/todo/zenkit-todo-linux.deb
+aria2c https://static.zenkit.com/downloads/desktop-apps/hypernotes/hypernotes-linux.deb
