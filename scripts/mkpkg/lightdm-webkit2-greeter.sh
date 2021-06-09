fa044eccf1b64dcda354199453d0c9c85af57e04
Raul Dipeas <rauldipeas@mail.com>
2021-06-09 13:24:38 -0300

    ^packages ^mkpkg

--- scripts/mkpkg/lightdm-webkit2-greeter.sh
+++ scripts/mkpkg/lightdm-webkit2-greeter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 GLORIOUS_TAG=$(curl -su $GAPI_AUTH https://api.github.com/repos/manilarome/lightdm-webkit2-theme-glorious/releases|grep tag|head -n1|cut -d '"' -f4|sed 's/https:\/\/github.com\/manilarome\/lightdm-webkit2-theme-glorious\/releases\/tag\///g')
+#echo 'deb http://download.opensuse.org/repositories/home:/antergos/xUbuntu_17.10/ /'|sudo tee /etc/apt/sources.list.d/lightdm-webkit2-greeter.list
+#wget -O- https://download.opensuse.org/repositories/home:antergos/xUbuntu_17.10/Release.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/lightdm-webkit2-greeter.gpg
+#apt-fast update
+#apt-fast download lightdm-webkit2-greeter
 #aria2c https://download.opensuse.org/repositories/home:/antergos/xUbuntu_17.10/amd64/lightdm-webkit2-greeter_2.2.5-1+15.31_amd64.deb
-echo 'deb http://download.opensuse.org/repositories/home:/antergos/xUbuntu_17.10/ /'|sudo tee /etc/apt/sources.list.d/lightdm-webkit2-greeter.list
-wget -O- https://download.opensuse.org/repositories/home:antergos/xUbuntu_17.10/Release.key|gpg --dearmor|sudo tee /etc/apt/trusted.gpg.d/lightdm-webkit2-greeter.gpg
-apt-fast update
-apt-fast download lightdm-webkit2-greeter
+cp packages/lightdm-webkit2-greeter/lightdm-webkit2-greeter*.deb .
 dpkg-deb -x lightdm-webkit2-greeter*.deb lightdm-webkit2-greeter
 dpkg-deb -e lightdm-webkit2-greeter*.deb lightdm-webkit2-greeter/DEBIAN
 rm lightdm-webkit2-greeter*.deb lightdm-webkit2-greeter/DEBIAN/md5sums
