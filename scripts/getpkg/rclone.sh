397d817616836d4413c0f3b76b721d3b36b4059c
Raul Dipeas <rauldipeas@mail.com>
2021-06-21 18:17:37 -0300

    ^getpkg

--- scripts/getpkg/rclone.sh
+++ scripts/getpkg/rclone.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-aria2c https://downloads.rclone.org/rclone-current-linux-amd64.deb
+#aria2c https://downloads.rclone.org/rclone-current-linux-amd64.deb
+aria2c $(curl -su $GAPI_AUTH https://api.github.com/repos/rclone/rclone/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)
