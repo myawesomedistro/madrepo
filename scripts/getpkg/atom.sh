4353dde0ee68db2298accd76bd807e37918a3b3f
Raul Dipeas <rauldipeas@mail.com>
2021-06-25 05:26:58 -0300

    ^getpkg

--- scripts/getpkg/atom.sh
+++ scripts/getpkg/atom.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-aria2c -o atom.deb https://atom.io/download/deb
+#aria2c -o atom.deb https://atom.io/download/deb
+aria2c $(curl -su $GAPI_AUTH https://api.github.com/repos/atom/atom/releases|grep browser_download_url|grep download|grep amd64.deb|head -n1|cut -d '"' -f4)
