82b59d46e7726c37a6cbcd9a11e7c18d38b42af5
Raul Dipeas <rauldipeas@mail.com>
2021-07-05 16:27:10 -0300

    ^getpkg

--- scripts/getpkg/hardcode-tray.sh
+++ scripts/getpkg/hardcode-tray.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-echo 'deb http://download.opensuse.org/repositories/home:/SmartFinn:/hardcode-tray/Debian_10/ /'|sudo tee  /etc/apt/sources.list.d/hardcode-tray.list
-wget -O- https://download.opensuse.org/repositories/home:SmartFinn:hardcode-tray/Debian_10/Release.key|gpg --deamor|sudo tee /etc/apt/trusted.gpg.d/hardcode-tray.gpg>/dev/null
-apt-fast update
-cat /var/lib/apt/lists/*hardcode-tray*_Packages|grep ^Package:|awk '{print $2}'|sort -u|xargs apt-fast download
+#echo 'deb http://download.opensuse.org/repositories/home:/SmartFinn:/hardcode-tray/Debian_10/ /'|sudo tee  /etc/apt/sources.list.d/hardcode-tray.list
+#wget -O- https://download.opensuse.org/repositories/home:SmartFinn:hardcode-tray/Debian_10/Release.key|gpg --deamor|sudo tee /etc/apt/trusted.gpg.d/hardcode-tray.gpg>/dev/null
+#apt-fast update
+#cat /var/lib/apt/lists/*hardcode-tray*_Packages|grep ^Package:|awk '{print $2}'|sort -u|xargs apt-fast download
+aria2c http://download.opensuse.org/repositories/home:/SmartFinn:/hardcode-tray/Debian_10/all/$(curl -s http://download.opensuse.org/repositories/home:/SmartFinn:/hardcode-tray/Debian_10/all/|grep deb|cut -d '"' -f4|tail -n1)
